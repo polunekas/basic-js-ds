6830bc937594a791cd87beb699be6a076e1c69b9
polina <polune.kas@gmail.com>
2023-04-16 18:31:18 +0300

    binary

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -11,24 +11,41 @@ root=null
   root() {
     let result
 	result = this.root 
+	return result
     // remove line with error and write your code here
   }
 
+new_node(tree, add_node) {
+ if (add_node.data < tree.data) {
+      if (tree.left !== null) {this.new_node(tree.left, add_node) } 
+	  else  tree.left = add_node;
+    } else if (add_node.data > tree.data) {
+      if (tree.right !== null) {
+       this.new_node(tree.right, add_node);
+      } else tree.right = add_node
+    }
+}
+
   add(data) {
 let add_node = {data:data, left: null, right: null}
-new_node(tree, item) {
-	
+
+if (this.root === null) {
+      this.root = add_node;
+    } else {
+      this.new_node(this.root, add_node);
+    }
+
 }
     // remove line with error and write your code here
-  }
+  
 
   has(/* data */) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  find(data) {
+
     // remove line with error and write your code here
   }
 
